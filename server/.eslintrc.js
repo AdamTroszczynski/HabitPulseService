86ba0b28758d951298c9f10540dd8e56b986691a
Adam Troszczynski <adamtroszczynski@outlook.com>
2026-03-13 12:36:37 +0100

    initial commit

---
+++ server/.eslintrc.js
@@ -0,0 +1,19 @@
+export default {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'airbnb-base', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    requireConfigFile: false,
+  },
+};
